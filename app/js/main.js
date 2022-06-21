a4c5b91de566b36f349b5094a4ca0a9a416bbe7e
feitaN17 <rocker1337pro@gmail.com>
2022-06-21 15:27:40 +0500

    first commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,22 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+  const burgerBtn = document.querySelector('.burger-btn')
+  const burgerMenu = document.querySelector('.burger-menu')
+  const overlay = document.querySelector('.overlay')
+  burgerBtn.addEventListener('click', () => {
+    if (!burgerMenu.classList.contains('burger-menu--active')) {
+      document.querySelector('html').style.overflowY = 'hidden'
+    } else {
+      console.log(1)
+      document.querySelector('html').style.overflowY = 'auto'
+    }
+    burgerMenu.classList.toggle('burger-menu--active')
+    burgerBtn.classList.toggle('burger-btn--active')
+    overlay.classList.toggle('overlay--active')
+  })
+  overlay.addEventListener('click', () => {
+    burgerMenu.classList.remove('burger-menu--active')
+    burgerBtn.classList.remove('burger-btn--active')
+    overlay.classList.remove('overlay--active')
+    document.querySelector('html').style.overflowY = 'auto'
+  })
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,81 +1,185 @@
-const { src, dest, watch, parallel, series } = require("gulp");
+let preprocessor = 'sass',
+	fileswatch = 'html,htm,txt,json,md,woff,woff2'
 
-const scss = require("gulp-sass")(require("sass"));
-const concat = require("gulp-concat");
-const autoprefixer = require("gulp-autoprefixer");
-const uglify = require("gulp-uglify");
-const imagemin = require("gulp-imagemin");
-const del = require("del");
-const browserSync = require("browser-sync").create();
+import pkg from 'gulp'
+const { gulp, src, dest, watch, parallel, series } = pkg
+
+import browserSync from 'browser-sync'
+import bssi from 'browsersync-ssi'
+import ssi from 'ssi'
+import fileInclude from 'gulp-file-include'
+import webpackStream from 'webpack-stream'
+import webpack from 'webpack'
+import TerserPlugin from 'terser-webpack-plugin'
+import uglify from 'gulp-uglify'
+import gulpSass from 'gulp-sass'
+import dartSass from 'sass'
+import sassglob from 'gulp-sass-glob'
+const sass = gulpSass(dartSass)
+import postCss from 'gulp-postcss'
+import cssnano from 'cssnano'
+import autoprefixer from 'autoprefixer'
+import imagemin from 'gulp-imagemin'
+import concat from 'gulp-concat'
+import rsync from 'gulp-rsync'
+import del from 'del'
+
+import svgSprite from 'gulp-svg-sprite'
+import cheerio from 'gulp-cheerio'
+import replace from 'gulp-replace'
 
 function browsersync() {
-  browserSync.init({
-    server: {
-      baseDir: "app/",
-    },
-    notify: false,
-  });
-}
-
-function styles() {
-  return src("app/scss/style.scss")
-    .pipe(scss({ outputStyle: "compressed" }).on("error", scss.logError))
-    .pipe(concat("style.min.css"))
-    .pipe(
-      autoprefixer({
-        overrideBrowserslist: ["last 10 versions"],
-        grid: true,
-      })
-    )
-    .pipe(dest("app/css"))
-    .pipe(browserSync.stream());
+	browserSync.init({
+		server: {
+			baseDir: 'app/',
+			middleware: bssi({ baseDir: 'app/', ext: '.html' }),
+		},
+		ghostMode: { clicks: false },
+		notify: false,
+		online: true,
+		// tunnel: 'hjkhjlkwdauhwdwdlul', // Attempt to use the URL https://hjkhjlkwdauhwdwdlul.loca.lt
+	})
 }
 
 function scripts() {
-  return src(["node_modules/jquery/dist/jquery.js", "app/js/main.js"])
-    .pipe(concat("main.min.js"))
-    .pipe(uglify())
-    .pipe(dest("app/js"))
-    .pipe(browserSync.stream());
+	return src(['app/js/*.js', '!app/js/*.min.js'])
+		.pipe(
+			webpackStream(
+				{
+					mode: 'production',
+					performance: { hints: false },
+					plugins: [
+						new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery', 'window.jQuery': 'jquery' }), // jQuery (npm i jquery)
+					],
+					module: {
+						rules: [
+							{
+								test: /\.m?js$/,
+								exclude: /(node_modules)/,
+								use: {
+									loader: 'babel-loader',
+									options: {
+										presets: ['@babel/preset-env'],
+										plugins: ['babel-plugin-root-import'],
+									},
+								},
+							},
+						],
+					},
+					optimization: {
+						minimize: true,
+						minimizer: [
+							new TerserPlugin({
+								terserOptions: { format: { comments: false } },
+								extractComments: false,
+							}),
+						],
+					},
+				},
+				webpack
+			)
+		)
+		.on('error', function handleError() {
+			this.emit('end')
+		})
+		.pipe(concat('main.min.js'))
+		.pipe(dest('app/js'))
+		.pipe(browserSync.stream())
+}
+
+function styles() {
+	return src(['app/styles/**/*.*', '!app/styles/**/_*.*'])
+		.pipe(eval(`${preprocessor}glob`)())
+		.pipe(eval(preprocessor)())
+		.pipe(postCss([autoprefixer({ grid: 'autoplace' }), cssnano({ preset: ['default', { discardComments: { removeAll: true } }] })]))
+		.pipe(concat('style.min.css'))
+		.pipe(dest('app/css'))
+		.pipe(browserSync.stream())
+}
+
+function svgSprites() {
+	return src('app/images/icons/**.svg')
+		.pipe(
+			cheerio({
+				run: function ($) {
+					$('[fill]').removeAttr('fill')
+					$('[stroke]').removeAttr('stroke')
+					$('[style]').removeAttr('style')
+				},
+				parserOptions: {
+					xmlMode: true,
+				},
+			})
+		)
+
+		.pipe(replace('&gt;', '>'))
+
+		.pipe(
+			svgSprite({
+				mode: {
+					stack: {
+						sprite: '../sprite.svg',
+					},
+				},
+			})
+		)
+		.pipe(dest('app/images'))
+}
+
+const htmlInclude = () => {
+	return src(['app/html/*.html']) // Находит любой .html файл в папке "html", куда будем подключать другие .html файлы
+		.pipe(
+			fileInclude({
+				prefix: '@',
+				basepath: '@file',
+			})
+		)
+		.pipe(dest('app')) // указываем, в какую папку поместить готовый файл html
+		.pipe(browserSync.stream())
 }
 
 function images() {
-  return src("app/images/**/*.*")
-    .pipe(
-      imagemin([
-        imagemin.gifsicle({ interlaced: true }),
-        imagemin.mozjpeg({ quality: 75, progressive: true }),
-        imagemin.optipng({ optimizationLevel: 5 }),
-        imagemin.svgo({
-          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
-        }),
-      ])
-    )
-    .pipe(dest("dist/images"));
+	return src(['app/images/**/*.*']).pipe(imagemin()).pipe(dest('dist/images')).pipe(browserSync.stream())
 }
 
-function build() {
-  return src(["app/**/*.html", "app/css/style.min.css", "app/js/main.min.js"], {
-    base: "app",
-  }).pipe(dest("dist"));
+function buildcopy() {
+	return src(['app/**/*.html', 'app/css/style.min.css', 'app/js/main.min.js', 'app/images/**/*.*', 'app/fonts/**/*', '!app/html/**/*.html', '!app/components/**'], {
+		base: 'app/',
+	}).pipe(dest('dist'))
 }
 
-function clean() {
-  return del("dist");
+async function clean() {
+	del('dist/**/*', { force: true })
+}
+
+function deploy() {
+	return src('dist/').pipe(
+		rsync({
+			root: 'dist/',
+			hostname: 'username@yousite.com',
+			destination: 'yousite/public_html/',
+			// clean: true, // Mirror copy with file deletion
+			include: [
+				/* '*.htaccess' */
+			], // Included files to deploy,
+			exclude: ['**/Thumbs.db', '**/*.DS_Store'],
+			recursive: true,
+			archive: true,
+			silent: false,
+			compress: true,
+		})
+	)
 }
 
 function watching() {
-  watch(["app/scss/**/*.scss"], styles);
-  watch(["app/js/**/*.js", "!app/js/main.min.js"], scripts);
-  watch(["app/**/*.html"]).on("change", browserSync.reload);
+	watch(['app/html/**/*.html'], { usePolling: true }, htmlInclude)
+	watch(`app/styles/**/*`, { usePolling: true }, styles)
+	watch(['app/js/**/*.js', '!app/js/*.min.js'], { usePolling: true }, scripts)
+	watch(['app/images/icons/**.svg'], { usePolling: true }, svgSprites)
+	watch(`app/**/*.{${fileswatch}}`, { usePolling: true }).on('change', browserSync.reload)
 }
 
-exports.styles = styles;
-exports.scripts = scripts;
-exports.browsersync = browsersync;
-exports.watching = watching;
-exports.images = images;
-exports.clean = clean;
-exports.build = series(clean, images, build);
-
-exports.default = parallel(styles, scripts, browsersync, watching);
+export { scripts, styles, images, htmlInclude, svgSprites, deploy }
+export let assets = series(scripts, styles, images, htmlInclude, svgSprites)
+export let build = series(clean, images, scripts, styles, buildcopy)
+export default series(svgSprites, htmlInclude, styles, scripts, parallel(browsersync, watching))
